fceb3c165728402d731507caa4a75ffaf9013055
Jens Hellmann <jens.hellmann@arvato.com>
2017-09-28 17:13:49 +0200

    - Fighting with CellEdito in UI

--- TimeInfo.TimeInfoUi/Views/MainWindow.xaml.cs
+++ TimeInfo.TimeInfoUi/Views/MainWindow.xaml.cs
@@ -6,9 +6,13 @@
 
 namespace TimeTool.Views
 {
+  using System;
   using System.Windows;
 
   using TimeTool.ViewModels;
+
+  using Xceed.Wpf.DataGrid;
+  using Xceed.Wpf.Toolkit;
 
   /// <summary>
   /// Interaction logic for MainWindow.xaml
@@ -21,8 +25,33 @@ namespace TimeTool.Views
     public MainWindow()
     {
       this.InitializeComponent();
+
+      var editor = new CellEditor();
+      editor.EditTemplate = new DataTemplate(typeof(TimePicker));
+
+      if (this.timeTable.DefaultCellEditors.ContainsKey(typeof(DateTime)))
+      {
+        editor = this.timeTable.DefaultCellEditors[typeof(DateTime)];
+        editor.EditTemplate = new DataTemplate(new TimePicker());
+      }
+      else
+      {
+        editor = new CellEditor();
+        editor.EditTemplate = new DataTemplate(typeof(TimePicker));
+
+        this.timeTable.DefaultCellEditors.Add(typeof(DateTime), editor);
+      }
+
+      this.timeTable.Columns["StartTime"]
+          .CellEditor = editor;
     }
 
+    /// <summary>
+    /// Acts on the event that the main window is closed.
+    /// Saves current user edit values.
+    /// </summary>
+    /// <param name="sender">The object that triggered this event.</param>
+    /// <param name="e">Contains the event specific information.</param>
     private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
     {
       var viewModel = (MainWindowViewModel)this.DataContext;
